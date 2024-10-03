a95e5cf5bbe2c6f0e62ddb36f30e47af362dc4b1
JR-Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-10-03 15:09:23 +0100

    added a more complex example

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,10 +15,37 @@ export const observableRunnerFactory = (): ObservableRunner => {
     console.log('🎶 I am running my application 🎶')
     //TODO pull markdown files from blob storage
     //TODO run this directly instead of via yarn
-    const reason = await runProcessWithTimeout('yarn', ['build:observable'], {}, 10 * 60 * 1000)
+    const reason = await runProcessWithTimeout(
+      'yarn',
+      ['build:observable'],
+      {},
+      10 * 60 * 1000
+    )
     console.log(`🎶 I am done running my application 🎶: ${JSON.stringify(reason)}`)
     //TODO store html to blob storage
   }
+}
+
+function handleData(data: Buffer | string) {
+  try {
+    Buffer.isBuffer(data) && (data = data.toString())
+    data.split('\n').forEach((line) => {
+      if (!line) return
+      try {
+        JSON.parse(line) // verify if the data is already in JSON format
+        process.stdout.write(line)
+        process.stdout.write('\n')
+      } catch {
+        wrapLogLine(line)
+      }
+    })
+  } catch {
+    wrapLogLine(JSON.stringify(data))
+  }
+}
+
+function wrapLogLine(line: string) {
+  console.log(line)
 }
 
 function runProcessWithTimeout(
@@ -30,12 +57,12 @@ function runProcessWithTimeout(
   return new Promise((resolve, reject) => {
     const childProc = spawn(cmd, cmdArgs, { env: { ...process.env, ...extraEnv } })
 
-    childProc.stdout.on('data', (data: unknown) => {
-      // handleData(data, false, boundLogger)
+    childProc.stdout.on('data', (data: Buffer) => {
+      handleData(data)
     })
 
-    childProc.stderr.on('data', (data: unknown) => {
-      // handleData(data, true, boundLogger)
+    childProc.stderr.on('data', (data: Buffer) => {
+      handleData(data)
     })
 
     let timedOut = false
